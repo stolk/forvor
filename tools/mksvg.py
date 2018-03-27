a65495b4e909246df8d54ab328e72eeeb061af06
Bram Stolk <b.stolk@gmail.com>
2018-03-27 16:55:44 -0700

    Colour-code the area sizes.

--- tools/mksvg.py
+++ tools/mksvg.py
@@ -5,6 +5,18 @@ import os
 import random
 
 
+def area( poly, verts ) :
+	total = 0
+	sz = len(poly)
+	for i in range(sz) :
+		i0 = poly[ i ]
+		i1 = poly[ (i+1)%sz ]
+		v0 = verts[ i0 ]
+		v1 = verts[ i1 ]
+		total += ( v1[0]-v0[0] ) * ( v1[1]+v0[1] )
+	return total
+
+
 def output_polygons( polygons, verts, s ) :
 	for p in polygons:
 		v0 = verts[ p[ 0 ] ]
@@ -12,16 +24,21 @@ def output_polygons( polygons, verts, s ) :
 		for vi in p[1:] :
 			v = verts[ vi ]
 			print 'L %f %f ' % ( s*v[0], s*v[1] ),
-		r = chr( int( random.uniform(65, 70) ) )
-		g = chr( int( random.uniform(65, 70) ) )
-		b = chr( int( random.uniform(65, 70) ) )
-		rgb = r+g+b
-		print 'z" stroke="black" fill="#%s" stroke-width="0.08" />' % ( rgb,  )
+		areasize = abs( area( p, verts ) )
+#		r = chr( int( random.uniform(65, 70) ) )
+#		g = chr( int( random.uniform(65, 70) ) )
+#		b = chr( int( random.uniform(65, 70) ) )
+#		rgb = r+g+b
+		r = 0.1 / areasize
+		r = 255 if r > 255 else r
+		rgb = "%02x%02x%02x" % ( r,r,r )
+		print 'z" stroke="black" fill="#%s" stroke-width="0.04" />' % ( rgb,  )
 
 
 def output_points( pnts, s ) :
+	ln = 0.1
 	for p in pnts:
-		print '  <path d="M %f %f L %f %f M %f %f L %f %f" stroke="red" stroke-width="0.04" />' % ( s*p[0]-0.2,s*p[1], s*p[0]+0.2, s*p[1], s*p[0],s*p[1]-0.2, s*p[0],s*p[1]+0.2 )
+		print '  <path d="M %f %f L %f %f M %f %f L %f %f" stroke="red" stroke-width="0.01" />' % ( s*p[0]-ln,s*p[1], s*p[0]+ln, s*p[1], s*p[0],s*p[1]-ln, s*p[0],s*p[1]+ln )
 
 
 if __name__ == "__main__" :
